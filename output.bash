987fec6a3e964371198b098574b775b93d16a840
yorks-dev <63572464+yorks-dev@users.noreply.github.com>
2020-06-12 09:31:41 +0530

    Create output.bash

---
+++ output.bash
@@ -0,0 +1,242 @@
+[edward@Edward Importaint Knowledge]$ python main_file.py 
+Enter your word
+__00|^*^|00__
+            _
+           ___
+          __0__
+         __000__
+        __00|00__
+       __00|^|00__
+      __00|^*^|00__
+     __00|^*^*^|00__
+    __00|^*^|^*^|00__
+   __00|^*^|0|^*^|00__
+  __00|^*^|000|^*^|00__
+ __00|^*^|00_00|^*^|00__
+__00|^*^|00___00|^*^|00__
+ __00|^*^|00_00|^*^|00__
+  __00|^*^|000|^*^|00__
+   __00|^*^|0|^*^|00__
+    __00|^*^|^*^|00__
+     __00|^*^*^|00__
+      __00|^*^|00__
+       __00|^|00__
+        __00|00__
+         __000__
+          __0__
+           ___
+            _
+[edward@Edward Importaint Knowledge]$ python main_file.py 
+Enter your word
+/_00\                
+    /
+   /_/
+  /_0_/
+ /_000_/
+/_00\00_/
+ /_000_/
+  /_0_/
+   /_/
+    /
+[edward@Edward Importaint Knowledge]$ python main_file.py 
+Enter your word
+/_00|^*^|00_\
+            /
+           /_/
+          /_0_/
+         /_000_/
+        /_00|00_/
+       /_00|^|00_/
+      /_00|^*^|00_/
+     /_00|^*^*^|00_/
+    /_00|^*^|^*^|00_/
+   /_00|^*^|0|^*^|00_/
+  /_00|^*^|000|^*^|00_/
+ /_00|^*^|00_00|^*^|00_/
+/_00|^*^|00_\_00|^*^|00_/
+ /_00|^*^|00_00|^*^|00_/
+  /_00|^*^|000|^*^|00_/
+   /_00|^*^|0|^*^|00_/
+    /_00|^*^|^*^|00_/
+     /_00|^*^*^|00_/
+      /_00|^*^|00_/
+       /_00|^|00_/
+        /_00|00_/
+         /_000_/
+          /_0_/
+           /_/
+            /
+[edward@Edward Importaint Knowledge]$ python main_file.py 
+Enter your word
+/00|^**^|00/ 
+           /
+          /0/
+         /000/
+        /00|00/
+       /00|^|00/
+      /00|^*^|00/
+     /00|^***^|00/
+    /00|^**^**^|00/
+   /00|^**^|^**^|00/
+  /00|^**^|0|^**^|00/
+ /00|^**^|000|^**^|00/
+/00|^**^|00/00|^**^|00/
+ /00|^**^|000|^**^|00/
+  /00|^**^|0|^**^|00/
+   /00|^**^|^**^|00/
+    /00|^**^**^|00/
+     /00|^***^|00/
+      /00|^*^|00/
+       /00|^|00/
+        /00|00/
+         /000/
+          /0/
+           /
+[edward@Edward Importaint Knowledge]$ python main_file.py 
+Enter your word
+\00__|^*^|__00/         
+              \
+             \0\
+            \000\
+           \00_00\
+          \00___00\
+         \00__|__00\
+        \00__|^|__00\
+       \00__|^*^|__00\
+      \00__|^*^*^|__00\
+     \00__|^*^|^*^|__00\
+    \00__|^*^|_|^*^|__00\
+   \00__|^*^|___|^*^|__00\
+  \00__|^*^|__0__|^*^|__00\
+ \00__|^*^|__000__|^*^|__00\
+\00__|^*^|__00/00__|^*^|__00\
+ \00__|^*^|__000__|^*^|__00\
+  \00__|^*^|__0__|^*^|__00\
+   \00__|^*^|___|^*^|__00\
+    \00__|^*^|_|^*^|__00\
+     \00__|^*^|^*^|__00\
+      \00__|^*^*^|__00\
+       \00__|^*^|__00\
+        \00__|^|__00\
+         \00__|__00\
+          \00___00\
+           \00_00\
+            \000\
+             \0\
+              \
+[edward@Edward Importaint Knowledge]$ python main_file.py 
+Enter your word
+\00__|^*^|__00\
+              \
+             \0\
+            \000\
+           \00_00\
+          \00___00\
+         \00__|__00\
+        \00__|^|__00\
+       \00__|^*^|__00\
+      \00__|^*^*^|__00\
+     \00__|^*^|^*^|__00\
+    \00__|^*^|_|^*^|__00\
+   \00__|^*^|___|^*^|__00\
+  \00__|^*^|__0__|^*^|__00\
+ \00__|^*^|__000__|^*^|__00\
+\00__|^*^|__00\00__|^*^|__00\
+ \00__|^*^|__000__|^*^|__00\
+  \00__|^*^|__0__|^*^|__00\
+   \00__|^*^|___|^*^|__00\
+    \00__|^*^|_|^*^|__00\
+     \00__|^*^|^*^|__00\
+      \00__|^*^*^|__00\
+       \00__|^*^|__00\
+        \00__|^|__00\
+         \00__|__00\
+          \00___00\
+           \00_00\
+            \000\
+             \0\
+              \
+[edward@Edward Importaint Knowledge]$ i_am_ayush
+bash: i_am_ayush: command not found
+[edward@Edward Importaint Knowledge]$ python main_file.py 
+Enter your word
+i_am_ayush
+         i
+        i_i
+       i_a_i
+      i_ama_i
+     i_am_ma_i
+    i_am_a_ma_i
+   i_am_aya_ma_i
+  i_am_ayuya_ma_i
+ i_am_ayusuya_ma_i
+i_am_ayushsuya_ma_i
+ i_am_ayusuya_ma_i
+  i_am_ayuya_ma_i
+   i_am_aya_ma_i
+    i_am_a_ma_i
+     i_am_ma_i
+      i_ama_i
+       i_a_i
+        i_i
+         i
+[edward@Edward Importaint Knowledge]$ i_am___ayush
+bash: i_am___ayush: command not found
+[edward@Edward Importaint Knowledge]$ python main_file.py 
+Enter your word
+i_am____ayush             
+            i
+           i_i
+          i_a_i
+         i_ama_i
+        i_am_ma_i
+       i_am___ma_i
+      i_am_____ma_i
+     i_am_______ma_i
+    i_am____a____ma_i
+   i_am____aya____ma_i
+  i_am____ayuya____ma_i
+ i_am____ayusuya____ma_i
+i_am____ayushsuya____ma_i
+ i_am____ayusuya____ma_i
+  i_am____ayuya____ma_i
+   i_am____aya____ma_i
+    i_am____a____ma_i
+     i_am_______ma_i
+      i_am_____ma_i
+       i_am___ma_i
+        i_am_ma_i
+         i_ama_i
+          i_a_i
+           i_i
+            i
+[edward@Edward Importaint Knowledge]$ lol___ayush
+bash: lol___ayush: command not found
+[edward@Edward Importaint Knowledge]$ python main_file.py 
+Enter your word
+lol____ayush
+           l
+          lol
+         lolol
+        lol_lol
+       lol___lol
+      lol_____lol
+     lol_______lol
+    lol____a____lol
+   lol____aya____lol
+  lol____ayuya____lol
+ lol____ayusuya____lol
+lol____ayushsuya____lol
+ lol____ayusuya____lol
+  lol____ayuya____lol
+   lol____aya____lol
+    lol____a____lol
+     lol_______lol
+      lol_____lol
+       lol___lol
+        lol_lol
+         lolol
+          lol
+           l
+[edward@Edward Importaint Knowledge]$ 
+
